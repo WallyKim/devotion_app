d3ac8e6aa50f63d701701964a471834a72a4751a
Wally <wkkim4@gmail.com>
2012-07-08 17:15:00 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevotionApp::Application.config.secret_token = 'f7366427d0a0d65393b1939719a70b8ab5326f3e26ddc561a39b4dc33ae0a2570f46f4594687fc20705f703ad542f5cc40a802ba20a164eb4559a91072f0b651'
