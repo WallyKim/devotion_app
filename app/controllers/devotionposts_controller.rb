9a2fbaf4a1e0db679200d8d1602adacff58a91f1
WallyKim <wkkim4@gmail.com>
2012-07-22 02:15:33 +0900

    create-form

--- app/controllers/devotionposts_controller.rb
+++ app/controllers/devotionposts_controller.rb
@@ -2,17 +2,26 @@ class DevotionpostsController < ApplicationController
   before_filter :signed_in_user, only: [:edit, :create, :destroy]
   before_filter :correct_user,   only: :destroy
   
+  def new
+    @category_id = params[:id]
+    @devotionpost = Devotionpost.new
+  end
+  
   def create
-    @category = Category.find(params[:category_id])
+    @category_id = params[:devotionpost][:bible_id]
+    @category = Category.find(@category_id)
     @devotionpost = @category.devotionposts.create(params[:devotionpost])
     # @devotionpost.bible_id = @category.bible_id
+    
+    @introlength = @devotionpost.intro.length
+    @contentlength = @devotionpost.content.length
+    @applicationlength = @devotionpost.application.length
     
     if @devotionpost.save
       # redirect_to root_path
       redirect_to @category, notice: 'Post was successfully created.'
     else
-      flash[:error] = @devotionpost.errors.full_messages
-      redirect_to @category
+      render action: "new"
     end
   end
   
@@ -23,6 +32,10 @@ class DevotionpostsController < ApplicationController
   def update
     @devotionpost = Devotionpost.find(params[:id])
     @category = Category.find(@devotionpost.category_id)
+    
+    @introlength = @devotionpost.intro.length
+    @contentlength = @devotionpost.content.length
+    @applicationlength = @devotionpost.application.length
 
     respond_to do |format|
       if @devotionpost.update_attributes(params[:devotionpost])
--- app/models/devotionpost.rb
+++ app/models/devotionpost.rb
@@ -2,9 +2,11 @@ class Devotionpost < ActiveRecord::Base
   belongs_to :category
   attr_accessible :address, :application, :bible_id, :content, :title, :intro
   
-  validates :intro, :length => { :maximum => 140 }
-  validates :content, :length => { :maximum => 140 }
-  validates :application, :length => { :maximum => 140 }
+  validates :title, presence: true
+  validates :address, presence: true
+  validates :intro, :length => { :maximum => 200 }, presence: true
+  validates :content, :length => { :maximum => 200 }, presence: true
+  validates :application, :length => { :maximum => 200 }, presence: true
   
   default_scope order: 'devotionposts.created_at DESC'
 end
